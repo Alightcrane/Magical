862a487592fec74e7ee196640edf595b80327249
renato <renato>
2016-07-07 20:24:43 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php incude_once("new html.html") ?>
+<?php incude_once("A better world.html") ?>
+<?php incude_once("teleportation.html") ?>
+<?php incude_once("real x-men.html") ?>
+<?php incude_once("Markus Persson.html") ?>
+<?php incude_once("Brothers.html") ?>
