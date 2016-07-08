2d70a74dd6596c1e8e807354c1d655928c9ddb67
Alightcrane <enriquesanchez254@gmail.com>
2016-07-07 20:39:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php incude_once("new html.html")?>
-<?php incude_once("A better world.html")?>
-<?php incude_once("teleportation.html")?>
-<?php incude_once("real x-men.html")?>
-<?php incude_once("Markus Persson.html")?>
-<?php incude_once("Brothers.html")?>
+<?php include_once("new html.html")?>
+<?php include_once("A better world.html")?>
+<?php include_once("teleportation.html")?>
+<?php include_once("real x-men.html")?>
+<?php include_once("Markus Persson.html")?>
+<?php include_once("Brothers.html")?>
 <?php include_once("new css.css")?>
